3600e9dfce7a1590ba58e36493e323c495377464
Pedro Melo <20182445@uac.pt>
2021-01-22 22:49:43 -0100

    Q Table feita

--- client/agent_rl_base.py
+++ client/agent_rl_base.py
@@ -8,15 +8,15 @@ def episode(c, res: int):
     if res != -1:
         msg = c.execute("info", "goal")
         goal = ast.literal_eval(msg)
-        directions = ["left", "right", "forward"]
-        msg = c.execute("info", "position")
-        pos = ast.literal_eval(msg)
+        commands = ["left", "right", "forward"]
         path = []
-        path.append(pos)
         end = False
         while not end:
             msg = c.execute("info", "view")
             objects = ast.literal_eval(msg)
+            msg = c.execute("info", "position")
+            pos = ast.literal_eval(msg)
+            command = random.choice(commands)
             if objects[0] == 'obstacle' or objects[0] == 'bomb':
                 c.execute("command", "left")
             else:
@@ -24,12 +24,13 @@ def episode(c, res: int):
                     print('GOAL found!')
                     end = True
                 else:
-                    direction = random.choice(directions)
-                    c.execute('command', direction)
-                    msg = c.execute("info", "position")
-                    pos = ast.literal_eval(msg)
-                    if direction == 'forward':
+                    if command == 'forward':
+                        direction = c.execute("info", "direction")
+                        pos = list(pos)
+                        pos.append(direction)
+                        pos = tuple(pos)
                         path.append(pos)
+                    c.execute('command', command)
 
         print('Path:\n', path)
         return path
@@ -37,15 +38,18 @@ def episode(c, res: int):
 
 # Função para atualizar tabela Q learning.
 # Recebe como argumentos a respetiva tabela e o percurso feito pelo agente.
-def updateQTable(QTable, path):
-    prevPos = path[len(path)-1]  # Posição do goal.
-    prevX, prevY = prevPos[0], prevPos[1]
-    for i in range(len(path)-2, -1, -1):
+def updateQTable(QTable, path, c):
+    msg = c.execute("info", "goal")
+    goal = ast.literal_eval(msg)
+    lastX, lastY, lastDirection = goal[0], goal[1], 'north'
+    for i in range(len(path)-1, -1, -1):
         newPos = path[i]
-        x, y = newPos[0], newPos[1]
-        if QTable[x][y] < 0.9*QTable[prevX][prevY]:
-            QTable[x][y] = 0.9*QTable[prevX][prevY]
-            prevX, prevY = x, y
+        x, y, direction = newPos[0], newPos[1], newPos[2]
+
+        q = 0.9*QTable[lastX][lastY][lastDirection]
+        if q > QTable[x][y][direction]:
+            QTable[x][y][direction] = q
+            lastX, lastY, lastDirection = x, y, direction
 
     print('QTable:')
     for column in QTable:
@@ -67,17 +71,22 @@ def main():
     for n in range(max_coord[0]):
         QTable += [[0] * max_coord[1]]
 
+    for column in range(len(QTable)):
+        for row in range(len(QTable[column])):
+            QTable[column][row] = {'north': 0, 'south': 0, 'east': 0, 'west': 0}
+
     msg = c.execute("info", "goal")
     goal = ast.literal_eval(msg)
-    QTable[goal[0]][goal[1]] = 100  # Reward do Goal igual a 100.
+    for direction in ['north', 'south', 'east', 'west']:
+        QTable[goal[0]][goal[1]][direction] = 100  # Reward do Goal igual a 100.
     print(QTable)
 
     # Executar episódios.
-    numEpisodes = 100
+    numEpisodes = 1
     for n in range(numEpisodes):
         print(n + 1, 'º episode')
         path = episode(c, res)  # Realizar um episódio.
-        QTable = updateQTable(QTable, path)  # Atualizar matriz Q-learning.
+        QTable = updateQTable(QTable, path, c)  # Atualizar matriz Q-learning.
         c.execute("command", "home")  # Voltar ao ponto de partida após um episódio.
 
     # Depois de concluídos todos os episódios, efetuar trajeto óptimo.

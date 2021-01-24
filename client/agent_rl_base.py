f34a96233d17194952209c59231c79b9e26bfeed
20182445-uac-pt <20182445@uac.pt>
2021-01-24 01:55:44 -0100

    'Setas'

--- client/agent_rl_base.py
+++ client/agent_rl_base.py
@@ -1,39 +1,39 @@
 import client
 import ast
 import random
+import time
 
 
 # Função que executa um episódio.
 def episode(c, res: int):
-    if res != -1:
-        msg = c.execute("info", "goal")
-        goal = ast.literal_eval(msg)
-        commands = ["left", "right", "forward"]
-        path = []
-        end = False
-        while not end:
-            msg = c.execute("info", "view")
-            objects = ast.literal_eval(msg)
-            msg = c.execute("info", "position")
-            pos = ast.literal_eval(msg)
-            command = random.choice(commands)
-            if objects[0] == 'obstacle' or objects[0] == 'bomb':
-                c.execute("command", "left")
+    msg = c.execute("info", "goal")
+    goal = ast.literal_eval(msg)
+    commands = ["left", "right", "forward"]
+    path = []
+    end = False
+    while not end:
+        msg = c.execute("info", "view")
+        objects = ast.literal_eval(msg)
+        msg = c.execute("info", "position")
+        pos = ast.literal_eval(msg)
+        command = random.choice(commands)
+        if objects[0] == 'obstacle' or objects[0] == 'bomb':
+            c.execute("command", "left")
+        else:
+            if pos == goal:
+                print('GOAL found!')
+                end = True
             else:
-                if pos == goal:
-                    print('GOAL found!')
-                    end = True
-                else:
-                    if command == 'forward':
-                        direction = c.execute("info", "direction")
-                        pos = list(pos)
-                        pos.append(direction)
-                        pos = tuple(pos)
-                        path.append(pos)
-                    c.execute('command', command)
+                if command == 'forward':
+                    direction = c.execute("info", "direction") # Direção para a qual o agente está virado.
+                    pos = list(pos)
+                    pos.append(direction)
+                    pos = tuple(pos)
+                    path.append(pos)
+                c.execute('command', command)
 
-        print('Path:\n', path)
-        return path
+    print('Path:\n', path)
+    return path
 
 
 # Função para atualizar tabela Q learning.
@@ -42,14 +42,18 @@ def updateQTable(QTable, path, c):
     msg = c.execute("info", "goal")
     goal = ast.literal_eval(msg)
     lastX, lastY, lastDirection = goal[0], goal[1], 'north'
-    for i in range(len(path)-1, -1, -1):
+    size = len(path)
+    i = size-1
+    while i >= 0:
         newPos = path[i]
         x, y, direction = newPos[0], newPos[1], newPos[2]
-
         q = 0.9*QTable[lastX][lastY][lastDirection]
+
         if q > QTable[x][y][direction]:
             QTable[x][y][direction] = q
-            lastX, lastY, lastDirection = x, y, direction
+
+        lastX, lastY, lastDirection = x, y, direction
+        i -= 1
 
     print('QTable:')
     for column in QTable:
@@ -58,39 +62,55 @@ def updateQTable(QTable, path, c):
     return QTable
 
 
-# Main.
-def main():
-    c = client.Client('127.0.0.1', 50001)
-    res = c.connect()
-    random.seed()  # To become true random, a different seed is used! (clock time)
-
-    # Inicializar Q Table.
-    QTable = []
-    msg = c.execute("info", "maxcoord")
-    max_coord = ast.literal_eval(msg)
-    for n in range(max_coord[0]):
-        QTable += [[0] * max_coord[1]]
-
+def markArrows(QTable, c):
     for column in range(len(QTable)):
         for row in range(len(QTable[column])):
-            QTable[column][row] = {'north': 0, 'south': 0, 'east': 0, 'west': 0}
+            bestReward = 0
+            bestDirection = ''
+            for direction in ['north', 'south', 'east', 'west']:
+                reward = QTable[column][row][direction]
+                print('rew', reward)
+                if reward > bestReward:
+                    bestDirection = direction
+                    bestReward = reward
 
-    msg = c.execute("info", "goal")
-    goal = ast.literal_eval(msg)
-    for direction in ['north', 'south', 'east', 'west']:
-        QTable[goal[0]][goal[1]][direction] = 100  # Reward do Goal igual a 100.
-    print(QTable)
-
-    # Executar episódios.
-    numEpisodes = 1
-    for n in range(numEpisodes):
-        print(n + 1, 'º episode')
-        path = episode(c, res)  # Realizar um episódio.
-        QTable = updateQTable(QTable, path, c)  # Atualizar matriz Q-learning.
-        c.execute("command", "home")  # Voltar ao ponto de partida após um episódio.
-
-    # Depois de concluídos todos os episódios, efetuar trajeto óptimo.
-    # optimalPath(QTable, c, res)
+            print('ola',bestDirection, bestReward)
+            c.execute('marrow', bestDirection + ',' + str(row) + ',' + str(column))
+            #time.sleep(0.2)
 
 
-main()
+# Main.
+def main(numEpisodes):
+    c = client.Client('127.0.0.1', 50001)
+    res = c.connect()
+    if res != -1:
+        random.seed()  # To become true random, a different seed is used! (clock time)
+
+        # Inicializar Q Table.
+        QTable = []
+        msg = c.execute("info", "maxcoord")
+        max_coord = ast.literal_eval(msg)
+        for n in range(max_coord[0]):
+            QTable += [[0] * max_coord[1]]
+
+        for column in range(len(QTable)):
+            for row in range(len(QTable[column])):
+                QTable[column][row] = {'north': 0, 'south': 0, 'east': 0, 'west': 0}
+
+        msg = c.execute("info", "goal")
+        goal = ast.literal_eval(msg)
+        for direction in ['north', 'south', 'east', 'west']:
+            QTable[goal[0]][goal[1]][direction] = 100  # Reward do Goal igual a 100.
+
+        for n in range(numEpisodes):
+            print(n + 1, 'º episode')
+            path = episode(c, res)  # Realizar um episódio.
+            QTable = updateQTable(QTable, path, c)  # Atualizar matriz Q-learning.
+            c.execute("command", "home")  # Voltar ao ponto de partida após um episódio.
+
+        # Depois de concluídos todos os episódios, mostrar setas.
+        markArrows(QTable, c)
+        #time.sleep(20)
+
+
+main(numEpisodes=1)
